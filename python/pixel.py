a38989214345462c0f3980c0d4f93248a3b2d587
victor bouvard <victor.bouvard@ge1.iut-cachan.u-psud.fr>
2020-01-24 11:26:39 +0100

    webcam ok

---
+++ python/pixel.py
@@ -0,0 +1,15 @@
+import cv2
+import sys, os
+
+image = cv2.imread('jpeg2000-home.jpg')
+i=0
+
+height, width = image.shape[:2]
+print ('',height)
+print ('',width)
+n=width*height*3
+
+while i<n
+    
+
+    
